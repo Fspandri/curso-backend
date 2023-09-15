26b268f2efe89a018b7f54b2707fa3bef5de2959
fspandri <florencia.spandri@gmail.com>
2023-09-15 01:54:52 -0300

    Segunda Pre Entrega

--- segundaPreEntrega-backend/src/index.js
+++ segundaPreEntrega-backend/src/index.js
@@ -5,13 +5,14 @@ import { Server } from 'socket.io';
 import { __dirname } from './path.js';
 import mongoose from "mongoose";
 
+import path from 'path'
 import productRouter from './routes/products.routes.js'
 import cartRouter from './routes/cart.routes.js'
-
+import productModel from './models/products.models.js';
 
 
 const app = express();
-const PORT = 4000;
+const PORT = 8080;
 
 
 // Server
@@ -33,7 +34,119 @@ app.set('views', path.resolve(__dirname, './views')) //Defino la ruta de las vis
 
 // Conexion BD
 mongoose.connect(process.env.MONGO_URL)
-    .then(async () => console.log("DB Conectada"))
+    .then(async () => {
+        console.log("DB Conectada")
+        await productModel.create([
+            {
+                title: 'Alfajor de chocolate',
+                description: 'Alfajor de chocolate relleno de dulce de leche',
+                price: 350,
+                stock: 50,
+                category: 'Alfajores',
+                code: 'ALFAJOR001',
+                thumbnails: [],
+            },
+            {
+                title: 'Alfajor de dulce de leche',
+                description: 'Alfajor relleno de dulce de leche',
+                price: 250,
+                stock: 50,
+                category: 'Alfajores',
+                code: 'ALFAJOR002',
+                thumbnails: [],
+            },
+            {
+                title: 'Alfajor de membrillo',
+                description: 'Alfajor relleno de membrillo',
+                price: 300,
+                stock: 50,
+                category: 'Alfajores',
+                code: 'ALFAJOR003',
+                thumbnails: [],
+            },
+            {
+                title: 'Marquisse',
+                description: 'Torta Marquisse',
+                price: 8000,
+                stock: 10,
+                category: 'Tortas',
+                code: 'TORTA001',
+                thumbnails: [],
+            },
+            {
+                title: 'Cheesecake de Frutos Rojos',
+                description: 'Clasico cheesecake de frutos rojos',
+                price: 7500,
+                stock: 10,
+                category: 'Tortas',
+                code: 'TORTA002',
+                thumbnails: [],
+            },
+            {
+                title: 'Chocotorta',
+                description: 'Chocotorta XL',
+                price: 7800,
+                stock: 15,
+                category: 'Tortas',
+                code: 'TORTA003',
+                thumbnails: [],
+            },
+            {
+                title: 'Cheesecake Oreo',
+                description: 'Cheesecake de Oreo',
+                price: 8500,
+                stock: 10,
+                category: 'Tortas',
+                code: 'TORTA004',
+                thumbnails: [],
+            },
+            {
+                title: 'Cheesecake de dulce de leche',
+                description: 'Cheesecake de dulce de leche y oreo',
+                price: 8600,
+                stock: 10,
+                category: 'Tortas',
+                code: 'TORTA005',
+                thumbnails: [],
+            },
+            {
+                title: 'Cuadrados de coco',
+                description: 'Cuadrado de coco y dulce de leche',
+                price: 500,
+                stock: 20,
+                category: 'Finger Food',
+                code: 'FINGERF001',
+                thumbnails: [],
+            },
+            {
+                title: 'Mini Rogel',
+                description: 'capas crocantes con dulce de leche',
+                price: 900,
+                stock: 15,
+                category: 'Finger Food',
+                code: 'FINGERF002',
+                thumbnails: [],
+            },
+            {
+                title: 'Brownies',
+                description: 'Brownies de chocolate',
+                price: 600,
+                stock: 50,
+                category: 'Finger Food',
+                code: 'FINGERF003',
+                thumbnails: [],
+            },
+            {
+                title: 'Tarteleta de frutilla',
+                description: 'Tarteleta con frutillas y crema',
+                price: 600,
+                stock: 25,
+                category: 'Finger Food',
+                code: 'FINGERF004',
+                thumbnails: [],
+            },
+        ])
+    })
     .catch((error) => console.log("Error de conexion a MongoDB Atlas ", error))
 
 
@@ -61,10 +174,6 @@ io.on('connection', (socket) => {
 app.use(express.json());
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
-
-app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`)
-})
 
 
 // Handlebars
